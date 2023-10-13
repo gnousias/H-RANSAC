97fbf31e7db30e4c9971346f55fbb15d92129d12
George Nousias <94700816+gnousias@users.noreply.github.com>
2023-10-13 20:02:24 +0300

    Update H_RANSAC.m

--- matlab_implementation/H_RANSAC.m
+++ matlab_implementation/H_RANSAC.m
@@ -1,27 +1,6 @@
 function [f_final] = H_RANSAC(varargin)
 % ,fLib,inlierIdx,pairs,dist,idx,cConv,timesAccepted,bestIterConv,countEqualQ,selected_transfQ
-%[f inlierIdx] = ransac1( x,y,ransacCoef,funcFindF,funcDist )
-%	Use RANdom SAmple Consensus to find a fit from X to Y.
-%	X is M*n matrix including n points with dim M, Y is N*n;
-%	The fit, f, and the indices of inliers, are returned.
-%
-%	RANSACCOEF is a struct with following fields:
-%	minPtNum,iterNum,thDist,thInlrRatio
-%	MINPTNUM is the minimum number of points with whom can we
-%	find a fit. For line fitting, it's 2. For homography, it's 4.
-%	ITERNUM is the number of iteration, THDIST is the inlier
-%	distance threshold and ROUND(THINLRRATIO*n) is the inlier number threshold.
-%
-%	FUNCFINDF is a func handle, f1 = funcFindF(x1,y1)
-%	x1 is M*n1 and y1 is N*n1, n1 >= ransacCoef.minPtNum
-%	f1 can be of any type.
-%	FUNCDIST is a func handle, d = funcDist(f,x1,y1)
-%	It uses f returned by FUNCFINDF, and return the distance
-%	between f and the points, d is 1*n1.
-%	For line fitting, it should calculate the dist between the line and the
-%	points [x1;y1]; for homography, it should project x1 to y2 then
-%	calculate the dist between y1 and y2.
-%	Yan Ke @ THUEE, 20110123, xjed09@gmail.com
+
 if nargin==10
     funcFindF = varargin{6};
     funcDist = varargin{7};
