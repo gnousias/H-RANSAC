fc53a266d87f1ade39a0a3def21d91c8990a8c4f
George Nousias <94700816+gnousias@users.noreply.github.com>
2023-10-16 18:28:45 +0300

    Update calcIterations.m

--- matlab_implementation/calcIterations.m
+++ matlab_implementation/calcIterations.m
@@ -7,16 +7,6 @@
 function niter = calcIterations(nA,nB,pr,isDataLabeled)
     if isDataLabeled
         nes=4;
-%         Na = nA(1)+nA(2);
-%         Nb = nB(1)+nB(2);
-%         k=min(Na,Nb);
-%         Ck4=nchoosek(k,nes);
-%         CNa4=nchoosek(Na,nes);
-%         CNb4=nchoosek(Nb,nes);
-%         p1=Ck4./CNa4;
-%         p2=Ck4./CNb4;
-%         p0=p1.*p2.*(1./(nchoosek(k,nes)*0.36))*(1./factorial(nes));
-%         niter=round(log10(1-pr)./log10(1-p0))
         N1a = nA(1); N2a = nA(2);
         N1b = nB(1); N2b = nB(2);
         a=3;
